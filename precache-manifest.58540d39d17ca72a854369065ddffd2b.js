f0286066904fd184ab5aab276da8c9595b2b60f1
MOHANAPRANESWARAN <mohanapraneswaran@gmail.com>
2021-09-25 17:10:43 +0530

    Updates

---
+++ precache-manifest.58540d39d17ca72a854369065ddffd2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba174249cea2bdaa677f",
+    "url": "/Portfolio/static/css/main.11258931.chunk.css"
+  },
+  {
+    "revision": "ba174249cea2bdaa677f",
+    "url": "/Portfolio/static/js/main.ba174249.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "fc22fa5e79caccf5acbd26223c3e00d7",
+    "url": "/Portfolio/index.html"
+  }
+];
